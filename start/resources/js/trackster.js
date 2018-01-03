4328c912beba2b3df10c294bbf0bb63872548222
goodgoof <anjalidsharma8@gmail.com>
2018-01-03 13:03:11 -0500

    adding Ajax

--- start/resources/js/trackster.js
+++ start/resources/js/trackster.js
@@ -1,14 +1,16 @@
 $(document).ready(function(){
   $('#search').click(function(){
-    console.log('hello');
+    Trackster.searchTracksByTitle($('input').val() );
   });
+  $.ajax({url:"http://ws.audioscrobbler.com/2.0/?method=track.search&track=tiny&api_key=aba0b6c323c5640cb1869094ac55c629&format=json",
+     success: function(console.log(hehe))
+   });
 
 });
 
 
-
+const API_KEY = aba0b6c323c5640cb1869094ac55c629;
 var Trackster = {};
-
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
   Append each "row" to the container in the body to display all tracks.
